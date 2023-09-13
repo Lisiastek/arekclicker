994c4f5112f828fd797690477fbd5b26daad6896
Mikołaj Podkowiak <miksonek2006@gmail.com>
2023-09-14 00:15:54 +0200

    arekPS

--- js/arekrender.js
+++ js/arekrender.js
@@ -5,7 +5,9 @@ export function arekrender(Game){
 function renderinfounder(Game){
     // render number of coins
     let arekC = document.getElementById('arekC');
-    arekC.innerHTML;
+    arekC.innerHTML = Game.arek + " arków";
     // render information about amount of coins obtained after one click and after one sec
+    let arekPS = document.getElementById('perS');
+    arekPS.innerHTML = "na sekunde: "+ Game.plusarekclick;
 
 }
