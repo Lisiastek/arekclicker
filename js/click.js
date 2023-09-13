5b48f8c4d23b010fd1ed88bccfa88d7bb1dee994
lisiastek <patryklata11@gmail.com>
2023-09-14 00:03:55 +0200

    click.js render

--- js/click.js
+++ js/click.js
@@ -1,4 +1,5 @@
 // purpose of this is just reacting after clicking at "thing" to obtaining coins (thing here was used because we dont want to refer to actual person)
 export function click(Game){
     Game.earn(Game.plusarekclick);
+    Game.render();
 }
