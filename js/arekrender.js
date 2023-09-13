9eb1feb5dcaf4c9c51eb34c2a040e8ce77f479a4
Mikołaj Podkowiak <miksonek2006@gmail.com>
2023-09-14 00:09:28 +0200

    dupa

--- js/arekrender.js
+++ js/arekrender.js
@@ -5,7 +5,7 @@ export function arekrender(){
 function renderinfounder(){
     // render number of coins
     let arekC = document.getElementById('arekC');
-    arekC.innerHTML
+    arekC.innerHTML;
     // render information about amount of coins obtained after one click and after one sec
 
 }
