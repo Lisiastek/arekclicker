99f6a5063d57b3e8f5679408b24631a5bf5d1ec7
lisiastek <patryklata11@gmail.com>
2023-09-13 21:43:35 +0200

    cute piesek

--- js/alert.js
+++ js/alert.js
@@ -1,3 +1,8 @@
+export function acceptBlank(){
+    let temp = document.getElementById("blank");
+    temp.style.display = "none";
+}
+acceptBlank = acceptBlank
 export function alertShow(){
     const blank = document.createElement('div');
     blank.style.background = "rgba(0,0,0,0.75)";
@@ -17,12 +22,9 @@ export function alertShow(){
     const blankMessage = document.createElement('div');
     blank.appendChild(blankMessage);
 
-    function acceptBlank(){
-        let temp = document.getElementById("blank");
-        temp.style.display = "none";
-    }
 
-    blankMessage.innerHTML = `<h2>Informacja</h2>
+
+    blankMessage.innerHTML = `<h2 style='text-align: center;'>Informacja</h2><img alt='cutepiesek' src='img/cutepiesek.png' style='position: absolute;top: 30px;left: 50px; width: 80px; opacity: 0.5;transform: rotate(-28deg);'>
         <p>Gra jest w celach edukacyjnych, <b>nie ma na celu szkalowania żadnej osoby</b> w tym zwłaszcza osób tutaj przedstawionych,
         wszelakie zdjęcia oraz informacje tutaj są zamieszczone w ramach żartu, nie należy ich brać na poważnie. 
         <br/><br/>
@@ -33,6 +35,7 @@ export function alertShow(){
         <button id='buttonblankmessage' onclick='acceptBlank()' style='color:rgb(219, 111, 4);font-size: 25px;height: 60px;transition: 0.5s;width: 80%;margin: 0px auto;padding: 10px;outline: none; background: none;border: none; border: 1px solid rgb(5, 203, 117);'>Akceptuję</button>`;
 
     blankMessage.style.background = 'cyan';
+    blankMessage.style.position = 'relative';
     blankMessage.style.borderRadius = "10%";
     blankMessage.style.width = "50%";
     // blankMessage.style.height = "400px";
--- js/click.js
+++ js/click.js
@@ -1,3 +1,4 @@
+// purpose of this is just reacting after clicking at "thing" to obtaining coins (thing here was used because we dont want to refer to actual person)
 export function click(Game){
     Game.earn(1);
     alert(Game.arek);
