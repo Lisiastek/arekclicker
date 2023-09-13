b92a7e41f22687430a179305c12d5182ec4ecacc
lisiastek <patryklata11@gmail.com>
2023-09-13 20:42:22 +0200

    usprawienia alert.js

--- js/alert.js
+++ js/alert.js
@@ -1,21 +1,37 @@
 export function alertShow(){
     const blank = document.createElement('div');
     blank.style.background = "rgba(0,0,0,0.75)";
-    blank.style.backgroundFilter = 'blur(3px)';
     blank.style.height = "100vh";
     blank.style.width = "100vw";
     blank.style.zIndex = "5";
     blank.style.position = "fixed";
     blank.style.display = "flex";
-    blank.style.justifyContent = "center";
-    blank.style.alignContent = "center";
     blank.style.right = "0";
-    blank.style.left = "0";
+    blank.style.top = "0";
+    blank.id = "blank";
+    blank.style['backdrop-filter'] = 'blur(4px)';
+    blank.style['alignContent'] = "center";
+    blank.style['justifyContent'] = "center";
+
 
     const blankMessage = document.createElement('div');
     blank.appendChild(blankMessage);
 
-    blankMessage.innerHTML = "Test";
+    blankMessage.innerHTML = `<h2>Informacja</h2>
+        <p>Gra jest w celach edukacyjnych, <b>nie ma na celu szkalowania żadnej osoby</b> w tym zwłaszcza osób tutaj przedstawionych,
+        wszelakie zdjęcia oraz informacje tutaj są zamieszczone w ramach żartu, nie należy ich brać na poważnie. 
+        <br/><br/>
+        Dodatkowo strona zbiera pliki cookies które są wymagane do prawidłowego działania tej strony. Wszelakie pliki cookies są używane wyłącznie
+        do zapisu postępu w grze.
+        <br /></br >
+        <h3>Klikając "Akceptuję" obwieszczam że zapoznałem się z sprawami tutaj napisanymi i się z nimi zgadzam.<h3>
+        <button onclick='acceptblank()'></button>`;
+
+    blankMessage.style.background = 'cyan';
+    blankMessage.style.borderRadius = "10%";
+    blankMessage.style.width = "40%";
+    blankMessage.style.height = "15%";
+    blankMessage.style.padding = "0.3%";
 
 
     document.body.appendChild(blank);
