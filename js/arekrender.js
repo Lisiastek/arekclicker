8bcfa85f94d0396ad02dff4e1708f1d3bc2d1be4
Mikołaj Podkowiak <miksonek2006@gmail.com>
2023-09-14 00:06:15 +0200

    areknieprzeciąganie

--- js/arekrender.js
+++ js/arekrender.js
@@ -4,7 +4,8 @@ export function arekrender(){
 
 function renderinfounder(){
     // render number of coins
-    
+    let arekC = document.getElementById('arekC');
+    arekC.innerHTML
     // render information about amount of coins obtained after one click and after one sec
 
 }
