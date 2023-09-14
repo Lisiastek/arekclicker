52f362d36b122f21dbd40e9553c20fdeb230e6fb
lisiastek <patryklata11@gmail.com>
2023-09-14 18:09:46 +0200

    komentarze w index.js

--- js/index.js
+++ js/index.js
@@ -14,14 +14,14 @@ const arekclick = 'arekclick';
 
 // main class
 class GameCLASS{
-    _arek = 0;
+    _arek = 0; // amount of coins
     _arekgained = 0; // total amount of coins gained
-    _areklost = 0;
-    _plusarekclick = 1;
-    _plusareksec = 0;
+    _areklost = 0; // total amount of coins lost
+    _plusarekclick = 1; // amount of coins that you get after click
+    _plusareksec = 0; // amount of coins that you get after one sec 
     _cheat = false; // is cheats was used
-    _idletime = 0;
 
+    // earn particular amount of coins, amount means here amount of this coins
     earn(amount) {
         this._arek += amount;
         if(amount > 0)
@@ -29,11 +29,12 @@ class GameCLASS{
         else
         this._areklost += amount;
     }
+    // render everything that can be change (automatically doing every 100ms)
     render(){
         arekrender(Game);
     }
     
-
+    // safe way to access coins (arekCoins)
     get arek(){return this._arek;}
     set arek(value){
         if(value > 0)
@@ -41,13 +42,17 @@ class GameCLASS{
         else
             this._arek = 0;
     }
+
+    // safe way to access amount of coins that you get after click (arekCoins)
     get plusarekclick(){return this._plusarekclick;}
     set plusarekclick(value){
         if(value > 0)
             this._plusarekclick = Number(value);
         else
             this._plusarekclick = 0;
-    }   
+    } 
+
+    // safe way to access amount of coins that you get after one sec (arekCoins)
     get plusareksec(){return this._plusareksec;}
     set plusareksec(value){
         if(value > 0)
@@ -56,27 +61,33 @@ class GameCLASS{
             this._plusareksec = 0;
     }   
 
-    inactivityfunc(){
-        document.title = "AC: " + this._arek;
-    }
-    
+
     constructor(){
+        
         window.addEventListener("DOMContentLoaded", function(){
+            // adding on clicks
             document.getElementById(arekclick).onclick = function(){
                 click(Game)
             }; 
         })
+
+        // adding automatic updating
         setInterval(() => {
             every100ms(Game);
         }, 100);
+
+        // alert
         alertShow();
 
+        // idle page behavior
         document.addEventListener('visibilitychange',
         () => {
             if(document.hidden){
                 this._temptitle = document.title;
                 document.title = "AC: " + this._arek;
-                this._tempinactivityfunc = setInterval(this.inactivityfunc, 1000);
+                this._tempinactivityfunc = setInterval(() => {
+                    document.title = "AC: " + this._arek;
+                }, 1000);
             }
             else {
                 document.title = this._temptitle;
@@ -84,27 +95,33 @@ class GameCLASS{
             }
         });
 
+
     }
     
 
 }
 
-let Game = new GameCLASS();
-Game.render();
-// commands management
+let Game = new GameCLASS(); // declare game
+Game.render(); // first render to make game faster available
+
+// commands management (from console)
 window.game = {
+    // allows you to add particular amount of money through web browser
     earn(amount){
             Game._cheat = true;
             Game.earn(amount);
         },
+    // allows you to forcefully set amount of coins getting after one sec
     setPlusArekSec(amount){
         Game._cheat = true;
         Game.plusareksec = amount;
     },
+    // allows you to forcefully set amount of coins getting after one click
     setPlusArekClick(amount){
         Game._cheat = true;
         Game.plusarekclick = amount;
     },
+    // forcefully show alert that had you got at the beginning
     showAlert(){
         document.getElementById('blank').style.display = "flex";
     }
