e3e6b4bbcc9c3e7ef7d641f420fb81ee79b66d5d
lisiastek <patryklata11@gmail.com>
2023-09-14 18:56:35 +0200

    kolejne poprawki

--- js/arekrender.js
+++ js/arekrender.js
@@ -6,7 +6,7 @@ export function arekrender(Game){
 
 function renderArek(Game){
     let arek = document.getElementById('arekclick');
-    let newstring = `<img src='../img/${Game.costume}.png' alt='arek' style='width: 100%;height:100%;'>`;
+    let newstring = `<img src='./img/${Game.costume}.png' alt='arek'>`;
     if(newstring!= arek.innerHTML){
         arek.innerHTML = newstring;
     }
