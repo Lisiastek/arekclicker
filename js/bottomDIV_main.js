a92b130044bc2e6c4923bb5c5824f2d8aa53756b
lisiastek <patryklata11@gmail.com>
2023-09-16 17:02:41 +0200

    css bottom

--- js/bottomDIV_main.js
+++ js/bottomDIV_main.js
@@ -1,3 +1,16 @@
 export function renderMain(Game){
-    return "test";
+    let tempText = ""
+    function addToRender(text){
+        tempText += text;
+    }
+
+
+    addToRender(`
+        <div class="centerBottomLocation openLocaction" id="sala28" onclick="changeLocactionCenter('sala28');">
+            <img class="centerBottomLocationImage openLocactionImage" src="./img/sala28.png">
+            <h2 class="centerBottomLocationText openLocactionText">Sala 28</h2>
+        </div>
+    `);
+
+    return tempText;
 }
