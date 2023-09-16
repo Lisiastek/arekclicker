249d398fb00a5675f210f12258e36b8e2e70bb89
lisiastek <patryklata11@gmail.com>
2023-09-16 23:18:48 +0200

    28 and 56 at numbering section

--- js/arekrender.js
+++ js/arekrender.js
@@ -15,7 +15,14 @@ function renderArek(Game){
 function renderinfounder(Game){
     // render number of coins
     let arekC = document.getElementById('arekC');
-    arekC.innerHTML = Game.arek + " arków";
+    // arek render
+    let _temp = Game.arek + " arków";
+    if((_temp.includes("28"))||(_temp.includes("56"))){
+        _temp = _temp.replaceAll("28", `<p class="text28">28</p>`);
+        _temp = _temp.replaceAll("56", `<p class="text56">56</p>`);
+    }
+    if(arekC.innerHTML != _temp) arekC.innerHTML = _temp;
+    // arekC.innerHTML = Game.arek + " arków";
     // render information about amount of coins obtained after one click and after one sec
     let arekPS = document.getElementById('perS');
     arekPS.innerHTML = "na sekunde: "+ Game.plusarekclick;
