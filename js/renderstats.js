2887363362f1f715620f064b68b50e5a2bb3822f
lisiastek <patryklata11@gmail.com>
2023-09-19 19:14:12 +0200

    stats v2

--- js/renderstats.js
+++ js/renderstats.js
@@ -14,6 +14,30 @@ export function renderStats(Game){
         </label>
         <progress id="cpp" max="100" value="100">
         </div>       
+        <div>
+        <label for="heisenberg">
+            <img src="./img/heisenberg.png" width="25">
+        </label>
+        <progress id="heisenberg" max="100" value="100">
+        </div>  
+        <div>
+        <label for="energy">
+            <img src="./img/energy.png" width="25">
+        </label>
+        <progress id="energy" max="100" value="100">
+        </div>  
+        <div>
+        <label for="food">
+            <img src="./img/food.png" width="25">
+        </label>
+        <progress id="food" max="100" value="100">
+        </div>  
+        <div>
+        <label for="xp" style="color:white;">
+            5 LV (200/300)
+        </label>
+        <progress id="xp" max="100" value="100">
+        </div> 
     `;
 
     
