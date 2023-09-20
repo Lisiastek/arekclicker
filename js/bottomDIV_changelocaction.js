5c9791cc3808ffa3b47f5c662befcfeebffc197b
lisiastek <patryklata11@gmail.com>
2023-09-20 18:04:08 +0200

    gdy nie mozna kupic to ikona + atrybut scene do centerG

--- js/bottomDIV_changelocaction.js
+++ js/bottomDIV_changelocaction.js
@@ -1,6 +1,8 @@
 // function to change locaction in center div in bottom section
 export function changeLocaction(Game, locaction){
     Game._stateOfBottomCenter = locaction;
+    let div = document.getElementById("centerB");
+    div.setAttribute("scene", locaction);
 
     Game.render();
 }
